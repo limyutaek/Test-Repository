67f506732886dde6603d3df103d5ddbe5b44e4d2
limyutaek <limyt1103@gmail.com>
2025-12-17 14:33:44 +0900

    service, serviceImpl에 주석 추가

--- testGithub/src/com/kh/github/model/service/GithubService.java
+++ testGithub/src/com/kh/github/model/service/GithubService.java
@@ -1,5 +1,5 @@
 package com.kh.github.model.service;
 
 public class GithubService {
-
+	//tester 1에서 추가
 }
--- testGithub/src/com/kh/github/model/service/GithubServiceImpl.java
+++ testGithub/src/com/kh/github/model/service/GithubServiceImpl.java
@@ -1,5 +1,5 @@
 package com.kh.github.model.service;
 
 public class GithubServiceImpl {
-
+	//tester1에서 추가
 }
